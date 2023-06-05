169f8a2ae4e19611e2b3b2ab9fd84c48010c5c56
yuliiashurek <90210678+yuliiashurek@users.noreply.github.com>
2023-06-05 23:12:21 +0300

    Update MainController.cs

--- lab5/lab5/MainController.cs
+++ lab5/lab5/MainController.cs
@@ -1,4 +1,4 @@
-﻿using lab5.Subjects;
+using lab5.Subjects;
 
 namespace lab5;
 
@@ -8,13 +8,33 @@ public class MainController
     private static readonly object Lock = new();
 
     private readonly MainView _userView;
+    private readonly MainModel _userModel;
 
-    private MainController(MainView userView)
+    private MainController(MainView userView, MainModel userModel)
     {
         _userView = userView;
+        _userModel = userModel;
     }
 
-    public void UpdateViewSubjectList(List<Subject>? subjects)
+    
+    public void CreateSubjUserSubsciptions()
+    {
+        UpdateViewSubjectList(_userModel.CreateSubjects());
+        _userModel.CreateUsers();
+        _userModel.AddingSubscriptions();
+        CreatesAndFillCommandAndHistory();
+        _userModel.ChangeDemo();
+    }
+    
+    private void CreatesAndFillCommandAndHistory()
+    {
+        PrintConsole(
+            "Enter a number n (added to governmentBond, naturalGas'll increase in n percentages):");
+        double number = ReadDouble();
+        _userModel.CreatesAndFillCommandAndHistory(number);
+    }
+    
+    public void UpdateViewSubjectList(List<Subject> subjects)
     {
         _userView.PrintToConsoleSubjectList(subjects);
     }
@@ -32,12 +52,12 @@ public class MainController
     }
     
     
-    public static MainController GetInstance(MainView userView)
+    public static MainController GetInstance(MainView userView, MainModel mainModel)
     {
         if (_instance != null) return _instance;
         lock (Lock)
         {
-            _instance ??= new MainController(userView);
+            _instance ??= new MainController(userView, mainModel);
         }
 
         return _instance;
